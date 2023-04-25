2e7692f521827f5c29b873a0141e0761b60294c5
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-04-25 16:38:58 +0200

    Update AI version.py

--- Keylogger/AI version.py
+++ Keylogger/AI version.py
@@ -8,3 +8,5 @@ from tensorflow, keras, losses import sparse_categorical_crossentropy
 # Load the training records
 with open("training_logs.txt", "r") as f:
     training_logs = f.readlines()
+
+# We divide the records into examples of input (X) and output (y)
